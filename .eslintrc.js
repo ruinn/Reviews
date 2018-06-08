5bc402287cd30bd6f22e23c41207e9409512500a
Darwin Do <darwindo@outlook.com>
2018-06-08 08:37:08 -0700

    added plugins to transpile spread operators

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,19 @@ module.exports = {
   extends: 'airbnb',
   rules: {
     'no-console': 'off',
-    'no-underscore-dangle': ['error', { allow: ['_id', '__REDUX_DEVTOOLS_EXTENSION__'] }],
-    'no-plusplus': 'off',
+    'no-underscore-dangle': [
+      'error',
+      { allow: ['_id', '__REDUX_DEVTOOLS_EXTENSION__'] }
+    ],
     camelcase: 'off',
     'no-mixed-operators': 'off',
-    'react/jsx-filename-extension': 'off',
+    'react/jsx-filename-extension': 'off'
   },
   env: {
     browser: true,
     node: true
-  }
+  },
+  plugins: ['transform-object-rest-spread']
   //   parserOptions: {
   //     'ecmaVersion': 2017
   //   }
